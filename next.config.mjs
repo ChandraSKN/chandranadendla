9e880e1e07e01fffd44c8044c0754dbdf6bf48d1
chandrasknadendla <chandrasknadendla@gmail.com>
2025-05-19 13:17:53 +0530

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 // next.config.mjs
 const config = {
-  // tell Next.js to do a static export
   output: 'export',
+  assetPrefix: '/chandranadendla/',
 
-  // if your site is at https://<user>.github.io/my-nextjs-app,
-  // uncomment the next line so all asset URLs get prefixed correctly:
-  basePath: '/my-nextjs-app',
-
-  // optional: add trailing slashes if you want URLs like /about/ instead of /about
-  // trailingSlash: true,
+  // (optional) so /about/ becomes /about/index.html
+  trailingSlash: true,
 };
 
 export default config;
